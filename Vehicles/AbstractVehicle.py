937699b38789ac5f832d44070fe07dcaecc3cf3b
ribsthakkar <rishabh.thakkar@gmail.com>
2019-09-15 11:18:32 -0500

    Created AbstractVehicle interface/class

---
+++ Vehicles/AbstractVehicle.py
@@ -0,0 +1,36 @@
+class AbstractVehicle():
+    def __init__(self):
+        pass
+
+    def reservation_width(self) -> float:
+        pass
+
+    def get_intersection_origin(self):
+        pass
+
+    def get_intersection_destination(self):
+        pass
+
+    def get_intersection_departure(self):
+        pass
+
+    def get_intersection_trajectory(self):
+        pass
+
+    def get_trajectory_distance(self):
+        pass
+
+    def get_conflicts_reserved(self):
+        pass
+
+    def get_origin(self):
+        pass
+
+    def get_destination(self):
+        pass
+
+    def get_current_intersection(self):
+        pass
+
+    def get_next_intersection(self):
+        pass
