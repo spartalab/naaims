07c21d96bb5b47e1cd5939d732a4bcb04a69fc2c
ribsthakkar <rishabh.thakkar@gmail.com>
2019-11-09 19:31:20 -0600

    Fixed display typo
    Signed-off-by: ribsthakkar <rishabh.thakkar@gmail.com>

--- Util/DisplayIntersection.py
+++ Util/DisplayIntersection.py
@@ -4,19 +4,22 @@ from Network.Intersection import Intersection
 
 
 def display(intersection: Intersection):
-    lanes = set(intersection.incomingLanes.keys() + intersection.outgoingLanes.values())
-
+    lanes = list(intersection.incomingLanes.keys())
+    for ls in intersection.outgoingLanes.values():
+        for l in ls:
+            lanes.append(l)
+    print(lanes)
     plt.figure(figsize=(15, 16))
     for i, r in enumerate(lanes):
         #     if r['IO'] == 'I':
         plt.xlim(-70, 70)
         plt.ylim(-60, 70)
-        plt.arrow(r.trajectory.p0[0], r.trajectory.p0[1], r.trajectory.p2[1] - r.trajectory.p0[0], r.trajectory.p2[1] - r.trajectory.p0[1], head_width=1,
+        plt.arrow(r.trajectory.p0[0], r.trajectory.p0[1], r.trajectory.p2[0] - r.trajectory.p0[0], r.trajectory.p2[1] - r.trajectory.p0[1], head_width=1,
                   overhang=.5)
 
     for i, r in enumerate(intersection.outgoingLanes.keys()):
         # tail = lanes.loc[r['TAIL_ID'], ['HEAD_X', 'HEAD_Y']].values
         # head = lanes.loc[r['HEAD_ID'], ['TAIL_X', 'TAIL_Y']].values
-        plt.arrow(r.trajectory.p0[0], r.trajectory.p0[1], r.trajectory.p2[1] - r.trajectory.p0[0], r.trajectory.p2[1] - r.trajectory.p0[1], head_width=1, overhang=1, color='r')
+        plt.arrow(r.trajectory.p0[0], r.trajectory.p0[1], r.trajectory.p2[0] - r.trajectory.p0[0], r.trajectory.p2[1] - r.trajectory.p0[1], head_width=1, overhang=1, color='r')
 
     plt.show()
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def main():
     sim = ArcAim(pd.read_csv("intersection_trajectory.csv", delimiter=','), pd.read_csv("lanes.csv", delimiter=',')) # TODO: sanitize ArcAIM inputs
     display(sim.intersection)
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
