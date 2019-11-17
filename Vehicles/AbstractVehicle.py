a0e748fb171955ae6b8b8cb4abd99c581e5ac439
Carlin Liao <carlin.liao@berkeley.edu>
2019-11-17 15:54:30 -0600

    added relevant properties to AbstractVehicle

--- Vehicles/AbstractVehicle.py
+++ Vehicles/AbstractVehicle.py
@@ -1,6 +1,29 @@
 class AbstractVehicle():
-    def __init__(self):
-        pass
+
+    # initialize some vehicle properties not in the constructor
+    a = 0 # zero m/s^2
+    p = 0 # progress
+
+    def __init__(self,
+        origin_lane, # lane in which this vehicle originates
+        destination_lane, # lane that this vehicle wants to exit from
+        a_max = 3, # maximum acceleration, in m/s^2
+        b_max = 3.4, # maximum (comfortable) braking, in m/s^2 (4.5 for uncomfortable braking)
+        l = 4.5, # length in meters
+        w = 3, # width in meters
+        v = 11 # vehicle speed, in meters per second
+        ):
+        '''
+        Construct a vehicle instance.
+        '''
+        
+        self.origin_lane = origin_lane
+        self.destination_lane = destination_lane
+        self.a_max = a_max
+        self.b_max = b_max
+        self.l = l
+        self.w = w
+        self.v = v
 
     def reservation_width(self):
         pass
